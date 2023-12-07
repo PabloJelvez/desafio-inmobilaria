677a6adb54bf46914dbad737f720a3a1bc8a55bc
Pablo Jelvez <pjelvezj@gmail.com>
2023-12-07 13:00:25 -0300

    Implementacion ciclos

--- assets/js/en_alquiler.js
+++ assets/js/en_alquiler.js
@@ -48,3 +48,36 @@ const propiedades_alquiler = [
         pets: false
      }
 ]
+
+let html = ""
+for(let propiedades of propiedades_alquiler){
+   
+   html +=`
+   <div class="col-md-4 mb-4">
+   <div class="card">
+   <img src="${propiedades.src}" class="card-img-top" alt="Imagen del departamento">
+   <div class="card-body">
+   <h5 class="card-title">${propiedades.nombre}</h5>
+   <p class="card-text">${propiedades.descripcion}</p>
+   <p> <i class="fas fa-map-marker-alt"></i> ${propiedades.ubicacion}</p>
+   <p> <i class="fas fa-bed"></i> ${propiedades.habitaciones} |
+   <i class="fas fa-bath"></i> ${propiedades.baños}</p>
+   <p> <i class="fas fa-dollar-sign"></i> ${propiedades.costo}</p>
+   `
+   if(propiedades.smoke){
+      html += `<p class="text-success"> <i class="fas fa-smoking"></i> Permitido fumar</p>`
+   } else {
+      html += `<p class="text-danger"> <i class="fas fa-smoking-ban"></i> No se permite fumar</p>`
+   }
+   if(propiedades.pets){
+      html += `<p class="text-success"> <i class="fas fa-paw"></i> Mascotas permitidas</p>`
+   } else {
+      html += `<p class="text-danger"> <i class="fas fa-ban"></i> No se permiten mascotas</p>`
+   }
+   html += `</div></div></div>`
+
+
+   const ele = document.getElementById("row")
+   ele.innerHTML = html
+
+}
--- assets/js/en_venta.js
+++ assets/js/en_venta.js
@@ -36,16 +36,62 @@ const propiedades_venta = [
      },
 
      {
-        nombre: "Penthouse de lujo con terraza panorámica",
-        src: "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
-        descripcion: "Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares",
-        ubicacion: "567 Skyline Avenue, Skyview City, CA 56789",
-        habitaciones: "3 Habitaciones",
+        nombre: "Hermosa casa en los suburbios",
+        src: "https://images.unsplash.com/photo-1600047509807-ba8f99d2cdde?q=80&w=1892&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        descripcion: "Esta hermosa casa esta ubicada en un gran barrio, muy tranquilo y seguro",
+        ubicacion: "867 Green Avenue, Springfield City, CA 85789",
+        habitaciones: "4 Habitaciones",
         baños: "3 baños",
-        costo: 4500,
-        smoke: false,
+        costo: 8500,
+        smoke: true,
         pets: true
-     }
+     },
+
+     {
+      nombre: "Condominio privado",
+      src: "https://images.unsplash.com/photo-1600585153490-76fb20a32601?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      descripcion: "Hermoso y moderna casa ubicada en exclusivo condominio.",
+      ubicacion: "666 Black Mountain,  Los Angeles, CA 85789",
+      habitaciones: "5 Habitaciones",
+      baños: "4 baños",
+      costo: 10500,
+      smoke: true,
+      pets: false
+   }
 
 
 ]
+
+let html = ""
+for(let propiedades of propiedades_venta){
+   
+   html +=`
+   <div class="col-md-4 mb-4">
+   <div class="card">
+   <img src="${propiedades.src}" class="card-img-top" alt="Imagen del departamento">
+   <div class="card-body">
+   <h5 class="card-title">${propiedades.nombre}</h5>
+   <p class="card-text">${propiedades.descripcion}</p>
+   <p> <i class="fas fa-map-marker-alt"></i> ${propiedades.ubicacion}</p>
+   <p> <i class="fas fa-bed"></i> ${propiedades.habitaciones} |
+   <i class="fas fa-bath"></i> ${propiedades.baños}</p>
+   <p> <i class="fas fa-dollar-sign"></i> ${propiedades.costo}</p>
+   `
+   if(propiedades.smoke){
+      html += `<p class="text-success"> <i class="fas fa-smoking"></i> Permitido fumar</p>`
+   } else {
+      html += `<p class="text-danger"> <i class="fas fa-smoking-ban"></i> No se permite fumar</p>`
+   }
+   if(propiedades.pets){
+      html += `<p class="text-success"> <i class="fas fa-paw"></i> Mascotas permitidas</p>`
+   } else {
+      html += `<p class="text-danger"> <i class="fas fa-ban"></i> No se permiten mascotas</p>`
+   }
+   html += `</div></div></div>`
+
+
+   const ele = document.getElementById("row")
+   ele.innerHTML = html
+
+}
+
